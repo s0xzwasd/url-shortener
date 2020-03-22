44a294cbf427c57335ea2b56d3a314fbcd32c49c
Daniil Maslov <29286703+s0xzwasd@users.noreply.github.com>
2020-03-22 23:53:54 +0300

    fix: small fetch to cleanuri (#11)

--- src/redux/actions/fetchCleanuri.js
+++ src/redux/actions/fetchCleanuri.js
@@ -8,9 +8,18 @@ export default function fetchCleanuri(url) {
 
     const response = await fetch("https://cleanuri.com/api/v1/shorten", {
       method: "POST",
+      mode: "no-cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Origin": "*"
+      },
+      redirect: "follow",
+      referrer: "no-referrer",
       body: formData
-    });
-    const json = await response.json();
-    dispatch({ type: FETCH_CLEANURI, payload: json });
+    }).then(res => res.json());
+
+    dispatch({ type: FETCH_CLEANURI, payload: response });
   };
 }
