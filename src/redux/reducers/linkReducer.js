35d98c251679035fcd5ca606b65ffaf4f7c62e1b
Daniil Maslov <s0xzwasd@protonmail.com>
2020-03-24 08:47:03 +0300

    fix: delete cleanuri

--- src/redux/reducers/linkReducer.js
+++ src/redux/reducers/linkReducer.js
@@ -1,10 +1,10 @@
-import { FETCH_RELINK } from "../types";
+import { FETCH_LINK } from "../types";
 
 const initialState = {};
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_RELINK:
+    case FETCH_LINK:
       return {
         ...state,
         link: action.payload
